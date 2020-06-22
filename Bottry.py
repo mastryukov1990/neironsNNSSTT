2d0d0c468489c3564eb9d4b1d3a7d913642aca88
anat mas <mastryukov1990@gmail.com>
2020-06-23 00:45:25 +0300

    Now we can send style photos

--- Bottry.py
+++ Bottry.py
@@ -1,8 +1,9 @@
 import telebot
-from config import data_bot,setting
+from config import data_for_bot,setting, image_loader, imshow
 from neuralStyleTransfer import create_and_start
 import os
-
+import matplotlib.pyplot as plt
+from NST import imshow1
 if not os.path.exists('content_photos'):
     os.makedirs('content_photos')
 if not os.path.exists('style_photos'):
@@ -28,7 +29,8 @@ class BBB:
             if not src in self.srcs:
                 self.srcs.append(src)
         except Exception as e:
-            self.bot.reply_to(message, e)
+            print('no')
+
 
     def eho(self,message):
         if message.content_type == 'text':
@@ -41,24 +43,30 @@ class BBB:
 
     def start_NST(self,message):
         if message.text == 'NST':
+           setting['style_imgs']=[]
+           for scr in self.srcs:
+               setting['style_imgs'].append(image_loader(scr))
            create_and_start(setting)
 
     def photo(self,message):
         if message.text == 'NST':
             self.bot.send_photo(message.chat.id, open('замок.png', 'rb'));
+    def show(self,message):
+        if message.text == 'покажи':
+            self.bot.send_photo(message.chat.id, open(self.srcs[0], 'rb'));
 
 
     def repeat_all_messages(self,message):  # Название функции не играет никакой роли, в принципе
         self.eho(message)
         self.start_NST(message)
-
+        self.show(message)
         self.take_photo(message)
         self.photo(message)
         self.handle_docs_photo(message)
 
 
 print('start')
-r = BBB(data_bot)
+r = BBB(data_for_bot)
 @r.bot.message_handler(content_types=["text", 'photo'])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     r.repeat_all_messages(message)
--- main.py
+++ main.py
@@ -1,4 +1,1 @@
 
-
-bot = telebot.TeleBot(config.TOKEN)
-bot.infinity_polling()
