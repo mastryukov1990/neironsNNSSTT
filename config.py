98434feda003143fb39b8cf86570122e2acb6049
anat mas <mastryukov1990@gmail.com>
2020-06-23 20:58:48 +0300

    still for colab? change bot Path

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ cnn = models.vgg19(pretrained=True).features.to(device).eval()
 cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 Path ="C:/Users/ASUS/Dropbox/image1/"
-data_for_bot = {
+data_for_bot ={
 'PathS': "content/style_photos",
 'PathC' : "content/content_photos",
     'TOKEN':'1247559782:AAEp7BbaFG6O6ztARSpTpUdxcU7O_UGHcWU'
